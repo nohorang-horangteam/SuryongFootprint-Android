618f0f4fd11d067593e34deb200d9b844a5cfa37
flora7883 <flora7883@gmail.com>
2022-02-01 22:39:08 +0900

    FEAT layout 구현

---
+++ app/src/main/java/kr/co/nohorang/suryongfootprint/FindPasswordResultActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.nohorang.suryongfootprint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FindPasswordResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_password_result)
+    }
+}
