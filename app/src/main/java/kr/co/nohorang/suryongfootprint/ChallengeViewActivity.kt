04105b2bf6525308987da5e80d3dff7fb991c4bd
b1urrrr <b1urrr@naver.com>
2022-02-02 21:34:12 +0900

    [ADD] 버튼 연결

--- app/src/main/java/kr/co/nohorang/suryongfootprint/ChallengeViewActivity.kt
+++ app/src/main/java/kr/co/nohorang/suryongfootprint/ChallengeViewActivity.kt
@@ -1,11 +1,22 @@
 package kr.co.nohorang.suryongfootprint
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import kr.co.nohorang.suryongfootprint.databinding.ActivityChallengeViewBinding
 
 class ChallengeViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_challenge_view)
+        val binding = ActivityChallengeViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 종료 버튼 - 액티비티 종료 (클릭 안 됨)
+        binding.challengeViewExitBtn.setOnClickListener {
+            Toast.makeText(this,"버튼 클릭됨", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
--- app/src/main/java/kr/co/nohorang/suryongfootprint/MainActivity.kt
+++ app/src/main/java/kr/co/nohorang/suryongfootprint/MainActivity.kt
@@ -13,11 +13,58 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 오늘의 챌린지 텍스트 앞으로 이동
         binding.dailyChallengeText.bringToFront()
+
+
+        // 오늘의 챌린지 버튼 - 액티비티 이동
+        binding.dailyChallengeBtn.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 인기순 챌린지 버튼(1위 ~ 5위) - 액티비티 이동
+        binding.mainChallengeBtn1.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.mainChallengeBtn2.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.mainChallengeBtn3.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.mainChallengeBtn4.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            startActivity(intent)
+        }
+        binding.mainChallengeBtn5.setOnClickListener {
+            val intent = Intent(this, ChallengeActivity::class.java)
+            startActivity(intent)
+        }
 
         // 다른 챌린지 보기 버튼 - 액티비티 이동
         binding.viewMoreChallengeBtn.setOnClickListener {
             val intent = Intent(this, ChallengeViewActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 메인메뉴 버튼 - 액티비티 이동
+        binding.approvalMenuBtn.setOnClickListener {
+            val intent = Intent(this, ApprovalActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
+        binding.rankingMenuBtn.setOnClickListener {
+            val intent = Intent(this, RankingActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
         }
     }
